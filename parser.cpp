c123cd38af0b2bd84e905a3ea6fc5dc68b94a5ac
Ilya Elizarov <ilya.elizarov@tum.de>
2021-07-13 23:44:33 +0200

    Fixed a bug in parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -9,33 +9,49 @@ using namespace std;
 
 int main() {
 
-	string str = "(123 ;222), (43,  0), (70,33), (23,10)";
+	string edge_list_str = "(123 ,222), (43,  0), (70,33), (23,10)";
 
 	// Erase all spaces in the string
-	str.erase(remove(str.begin(), str.end(), ' '), str.end());	
-	cout << str << endl;
+	edge_list_str.erase(remove(edge_list_str.begin(), edge_list_str.end(), ' '), edge_list_str.end());	
+	cout << edge_list_str << endl;
 
 	// Splits the string into pairs for the format (number,number)
 	regex reg_pair("\\([0-9]+[,;][0-9]+\\)");
-	sregex_token_iterator pos_pair(str.cbegin(), str.cend(), reg_pair, 0);
-	sregex_token_iterator end_pair;
 
 	// Splits the pair into a pair of edges
 	regex reg_vertex("[0-9]+");
+
 	typedef pair<int, int> int_pair;
 	vector<int_pair> net_edges; 
-	// typedef vector<int_pair> vector_pair;
-	//vector_pair net_edges;
+	typedef vector<int_pair> vector_pair;
+	// vector_pair net_edges;
+	//
 
+	sregex_token_iterator pos_pair(edge_list_str.cbegin(), edge_list_str.cend(), reg_pair, 0);
+        sregex_token_iterator end_pair;
+
+	int i = 0;
 	for( ; pos_pair!=end_pair; ++pos_pair) {
-		string pair_str = *pos_pair;
-		sregex_token_iterator pos_edge(pair_str.cbegin(), pair_str.cend(), reg_vertex, 0);
 		
-		net_edges.push_back(make_pair(stoi(*pos_edge), stoi(*(++pos_edge))));
+		string pair_str = *pos_pair;
+		cout << ++i << endl;
+		cout << pair_str << endl;
+
+
+		sregex_token_iterator pos_edge(pair_str.begin(), pair_str.end(), reg_vertex, 0);
+//		sregex_token_iterator end_edge;
+
+//		cout << *(++pos_pair) << endl;
+
+		int m_firstVertex =  stoi(*pos_edge);
+//		pos_edge++;
+		int m_secondVertex = stoi(*(++pos_edge));
+
+		net_edges.push_back(make_pair(m_firstVertex, m_secondVertex));
 	}
 
-/*	for(vector_pair::iterator inet_edges = net_edges.begin();
-		inet_edges != net_edges.end(); ++inet_edges) { */
+/*	for(vector_pair::iterator inet_edge = net_edge.begin();
+		inet_edge != net_edge.end(); ++inet_edge) { */ 
 
 	for(int_pair inet_edge: net_edges) {
 		cout << "First vertex: " << inet_edge.first << endl;
