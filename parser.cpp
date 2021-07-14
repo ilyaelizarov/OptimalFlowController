a4dbd37e158baa2d0405e3b27034d04adf500d44
Ilya Elizarov <ilya.elizarov@tum.de>
2021-07-14 02:33:26 +0200

    Added a class with methods to parse strings with edges and lengths list

--- parser.cpp
+++ parser.cpp
@@ -1,4 +1,6 @@
-/* Parsers a string with edges, e.g. (1,2),(2,3) to a pair type */
+/* 1. Parsers a string with edges, e.g. (1,2),(2,3) to a pair of integers vector
+ * 2. Parsers a string with lengths, e.g. 1, 3.23; 4 to a double vector
+ */
 
 #include <iostream>
 #include <iterator>
@@ -7,56 +9,92 @@
 
 using namespace std;
 
+class ParseParameters {
+
+	private:
+        // Expression for splitting the string that describes edges list into pairs of numbers
+        const regex c_pair_reg{"\\([0-9]+[,;][0-9]+\\)"};
+
+	// Expression for splitting the pairs into vertexes
+        const regex c_vertex_reg{"[0-9]+"};
+
+	// Expression for splitting a string into numbers
+	const regex c_number_reg{"[,;]"};
+
+	public:
+		void EdgesList (string edge_list_str, vector<pair<int, int>> * edges_vector) {
+
+			// Erase all spaces in the string
+        		edge_list_str.erase(remove(edge_list_str.begin(), edge_list_str.end(), ' '), edge_list_str.end());
+
+			// Matching sequence iterator for the pair
+        		sregex_token_iterator i_pair(edge_list_str.cbegin(), edge_list_str.cend(), c_pair_reg, 0);
+        		sregex_token_iterator i_pair_last;
+
+			// Parsing every pair to get vertexes
+			for(; i_pair!=i_pair_last; ++i_pair) {
+
+				string pair_str = *i_pair;
+
+				// Matching sequence iterator for the vertex
+				sregex_token_iterator i_vertex(pair_str.cbegin(), pair_str.cend(), c_vertex_reg, 0);
+
+				int first_vertex = stoi(*i_vertex);
+				int second_vertex = stoi(*(++i_vertex));
+
+				edges_vector->push_back(make_pair(first_vertex, second_vertex));
+
+			}
+		}
+
+		void LengthList (string length_list_str, vector<double> * lengths_vector) {
+
+			// Erase all spaces in the string
+                        length_list_str.erase(remove(length_list_str.begin(), length_list_str.end(), ' '), length_list_str.end());
+
+			// Matching sequence iterator for the length
+                        sregex_token_iterator i_length(length_list_str.cbegin(), length_list_str.cend(), c_number_reg, -1);
+                        sregex_token_iterator i_length_last;
+
+
+			// Parsing every length number
+                        for(; i_length!=i_length_last; ++i_length) {
+
+                                double length = stod(*i_length);
+
+                                lengths_vector->push_back(length);
+
+                        }
+
+		}
+
+} ParseParameters;
+			
+
 int main() {
 
-	string edge_list_str = "(123 ,222), (43,  0), (70,33), (23,10)";
 
-	// Erase all spaces in the string
-	edge_list_str.erase(remove(edge_list_str.begin(), edge_list_str.end(), ' '), edge_list_str.end());	
-	cout << edge_list_str << endl;
+	// Tests methods in the ParseParameters class
 
-	// Splits the string into pairs for the format (number,number)
-	regex reg_pair("\\([0-9]+[,;][0-9]+\\)");
-
-	// Splits the pair into a pair of edges
-	regex reg_vertex("[0-9]+");
+	string edge_list_str = "(123 ,-222), (43,  0), (70,33), (23,10), (24,5)";
 
 	typedef pair<int, int> int_pair;
 	vector<int_pair> net_edges; 
-	typedef vector<int_pair> vector_pair;
-	// vector_pair net_edges;
-	//
 
-	sregex_token_iterator pos_pair(edge_list_str.cbegin(), edge_list_str.cend(), reg_pair, 0);
-        sregex_token_iterator end_pair;
+	ParseParameters.EdgesList(edge_list_str, &net_edges);
 
-	int i = 0;
-	for( ; pos_pair!=end_pair; ++pos_pair) {
-		
-		string pair_str = *pos_pair;
-		cout << ++i << endl;
-		cout << pair_str << endl;
+        for(int_pair i_net_edge: net_edges) {
+                cout << "First vertex: " << i_net_edge.first << endl;
+                cout << "Second vertex: " << i_net_edge.second << endl;
+        }
 
+	vector<double> net_lengths_method;
 
-		sregex_token_iterator pos_edge(pair_str.begin(), pair_str.end(), reg_vertex, 0);
-//		sregex_token_iterator end_edge;
+	ParseParameters.LengthList("-1, 2,4,5; 2.53, 10;175, 2.45, 2e3", &net_lengths_method);
 
-//		cout << *(++pos_pair) << endl;
-
-		int m_firstVertex =  stoi(*pos_edge);
-//		pos_edge++;
-		int m_secondVertex = stoi(*(++pos_edge));
-
-		net_edges.push_back(make_pair(m_firstVertex, m_secondVertex));
-	}
-
-/*	for(vector_pair::iterator inet_edge = net_edge.begin();
-		inet_edge != net_edge.end(); ++inet_edge) { */ 
-
-	for(int_pair inet_edge: net_edges) {
-		cout << "First vertex: " << inet_edge.first << endl;
-		cout << "Second vertex: " << inet_edge.second << endl;
-	}
-
+	for(double length: net_lengths_method) {
+                cout << "Length: " << length << endl;
+        }
+	
 	return 0;
 }	
