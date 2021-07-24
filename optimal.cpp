b59a97b9d53f9d44071959565fa9799dea0c5234
Ilya <ilya.elizarov@yahoo.com>
2021-07-24 23:33:59 +0200

    Fixed bugs and tested the code for making A_eq matrix for optimization problem

--- optimal.cpp
+++ optimal.cpp
@@ -1,61 +1,81 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+
 using namespace std;
 
-vector<double> m_flows = {1, 2, -2, -1};
+vector<double> m_flows = {2, 2, -2, -1};
 
+vector< vector<double> > A_eq;
 vector<double> A_eq_row = {0};
 
-m_flows::iterator begin_shift = m_flows.begin();
-
-A_eq_row.resize(m_flows.size());
-
-
-int i_prosumer = 0;
+int i_oddity = 0;
 int i_column = 0;
 
-for (/* Iterate over nodes, i_node is an iterator, initially i_node = 0 */ ) {
+int num_loops = 3;
 
-	i_oddity = 1;
-	i_column = i_node;
+int main(void) {
 
-	for (m_flows::iterator i_m_flow=begin_shift; i_m_flow != begin_shift + 2; i_m_flow++) {
+	vector<double>::iterator begin_shift = m_flows.begin();
 
-		// Consumption mode, every first branch
-		if ( (*i_m_flow > 0) && !(i_oddity % 2) ) {
+	A_eq_row.resize(m_flows.size());
 
-			A_eq_row.at(i_column) = -pow(*i_m_flow,2);
+	for (int i_loop = 0; i_loop != num_loops; i_loop++) {
 
-		// Consumption mode, every second branch
-		} else if ( (*i_m_flow > 0) && (i_oddity % 2) ) {
+		i_oddity = 1;
+		i_column = i_loop;
+
+		for (vector<double>::iterator i_m_flow=begin_shift; i_m_flow != begin_shift + 2; i_m_flow++) {
+
+			// Consumption mode, every first branch
+			if ( (*i_m_flow > 0) && !(i_oddity % 2) ) {
+
+				A_eq_row.at(i_column) = -pow(*i_m_flow,2);
+
+			// Consumption mode, every second branch
+			} else if ( (*i_m_flow > 0) && (i_oddity % 2) ) {
 		
-			A_eq_row.at(i_column) = pow(*i_m_flow,2);
+				A_eq_row.at(i_column) = pow(*i_m_flow,2);
 	
-		// Production mode, every first branch
-		} else if ( (*i_m_flow < 0) && !(i_oddity % 2) ) {
+			// Production mode, every first branch
+			} else if ( (*i_m_flow < 0) && !(i_oddity % 2) ) {
 
-			A_eq_row.at(i_column) = -1;
+				A_eq_row.at(i_column) = -1;
 
-		// Production mode, every second branch
-		} else {
+			// Production mode, every second branch
+			} else {
 
-			A_eq_row.at(i_column) = 1;
-		}
+				A_eq_row.at(i_column) = 1;
+			}
 	
 		i_oddity++;
 		i_column++;
 	
-	}
+		}
 
 
-	A_eq.push_back(A_eq_row);
+		A_eq.push_back(A_eq_row);
 
 	fill(A_eq_row.begin(), A_eq_row.end(), 0.0);
 
 	begin_shift++;
 
 
+	}
+
+for (vector< vector<double> >::iterator i_counter = A_eq.begin();
+	 i_counter != A_eq.end(); i_counter++) {
+
+	for (vector<double>::iterator i_row_counter = (*i_counter).begin(); i_row_counter != (*i_counter).end();
+		i_row_counter++) {
+
+		cout << *i_row_counter << ' ';
+
+	}
+
+	cout << endl;
+
 }
 
-struct OptimizationDataModel {
-	vector<vector<double>> A_eq {
-		{-1; 
 
+}
