75658e1e0cbc4feef5e90f464e40a85a2e89f942
Ilya Elizarov <ilya.elizarov@tum.de>
2021-07-29 22:50:49 +0200

    Added code for running an optimization solver from OR-Tools

--- optimal.cpp
+++ optimal.cpp
@@ -1,8 +1,13 @@
+#include "/home/wht/Documents/lib/or-tools_Ubuntu/include/ortools/linear_solver/linear_solver.h"
 #include <vector>
 #include <cmath>
 #include <iostream>
 
 using namespace std;
+
+struct OptimizationDataModel {
+	vector<vector<double>> cons
+
 
 vector<double> m_flows = {2, 2, -2, -1};
 
@@ -12,6 +17,7 @@ vector<double> A_eq_row = {0};
 int i_oddity = 0;
 int i_column = 0;
 
+// Number of loops
 int num_loops = 3;
 
 int main(void) {
@@ -63,6 +69,7 @@ int main(void) {
 
 	}
 
+// Print matrix A_eq
 for (vector< vector<double> >::iterator i_counter = A_eq.begin();
 	 i_counter != A_eq.end(); i_counter++) {
 
@@ -76,6 +83,48 @@ for (vector< vector<double> >::iterator i_counter = A_eq.begin();
 	cout << endl;
 
 }
+
+// Create a solver
+unique_ptr<MPSolver> solver(MPSolver::CreateSolver("SCIP"));
+
+// Define the decision variables
+const double infinity = solver->infinity();
+
+vector<const MPVariable*> x(4);
+for (int i_opt_var =0; i_opt_var < 4; ++i_opt_var) {
+	x[i_opt_var] = solver->MakeIntVar(0.0, infinity, "");
+}
+
+// Create the constraints
+vector<double> bounds{0, 0, 0};
+
+for (int i_constr = 0; i_constr < 3; ++i_constr) {
+	MPConstraint* constraint = solver->MakeRowConstraint(bounds[i_constr],
+			bounds[i_constr], "");
+	for (int i_constr_coef =0; i_constr_coef < 4; ++i_constr_coef) {
+		constraint->SetCoefficient(x[i_constr_coef],
+			       A_eq[i_constr][i_constr_coef]);
+	}
+}
+
+// Create objective function
+vector<double> obj_coeffs;
+
+obj_coeffs.push_back(m_flows[0]^3);
+obj_coeffs.push_back(m_flows[1]^3);
+obj_coeffs.push_back(m_flows[2]);
+obj_coeffs.push_back(m_flows[3]);
+
+MPObjective* objective = solver->MutableObjective();
+
+for (int i_obj; i_obj < 4; ++i_obj) {
+	objective->SetCoefficient(x[i_obj], obj_coeffs[i_obj]);
+}
+
+objective->SetMinimization();
+
+// Call the solver
+const MPSolver::ResultStatus result_status = solver->Solve();
 
 
 }
